84216150d51edcf25d379a9c227dd734823ab1ea
BTNGaming <kevdwg08@gmail.com>
2020-08-17 20:04:12 -0700

    Update fxmanifest.lua

--- resources/gcphone/fxmanifest.lua
+++ resources/gcphone/fxmanifest.lua
@@ -1,9 +1,10 @@
-fx_version 'bodacious'
-game 'gta5'
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
 
 author 'BTNGaming & chip (Original: n3mtv)'
 description 'GCPhone for ESX 1.2 and 1.1(or older)'
 version '1.1'
+
 
 ui_page 'html/index.html'
 
@@ -43,19 +44,31 @@ files {
 
 	'html/static/sound/*.ogg',
 	'html/static/sound/*.mp3',
+
 }
 
 client_script {
-	'@es_extended/locale.lua',
-	'config.lua',
-	'locales/*.lua'
-	'client/*.lua'
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+	"client/esxaddonsgcphone-c.lua",
+	"config.lua",
+	"client/animation.lua",
+	"client/client.lua",
+
+	"client/photo.lua",
+	"client/app_tchat.lua",
+	"client/bank.lua",
+	"client/twitter.lua"
 }
 
 server_script {
-	'@es_extended/locale.lua',
+    '@es_extended/locale.lua',
+    'locales/*.lua',
 	'@mysql-async/lib/MySQL.lua',
-	'config.lua',
-	'locales/*.lua',
-	'server/*.lua'
+	"server/esxaddonsgcphone-s.lua",
+	"config.lua",
+	"server/server.lua",
+
+	"server/app_tchat.lua",
+	"server/twitter.lua"
 }
