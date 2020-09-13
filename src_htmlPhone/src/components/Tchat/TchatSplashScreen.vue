263be952b91484074d6e0b6f3683116189b9b59e
RockySouthpaw <55056068+RockySouthpaw@users.noreply.github.com>
2020-09-12 21:50:20 -0400

    Dark Chat Entierly Redone
    https://cdn.discordapp.com/attachments/719393533131489301/754504463250882635/Dark_Net_Revamp.mp4 See video for current look.

--- src_htmlPhone/src/components/Tchat/TchatSplashScreen.vue
+++ src_htmlPhone/src/components/Tchat/TchatSplashScreen.vue
@@ -18,7 +18,7 @@ export default {
   .splash{
     width: 100%;
     height: 100%;
-    background-color: #20201d;
+    background-color: black; /** Splash Background */
     display: flex;
     justify-content: center;
     align-items: center;
