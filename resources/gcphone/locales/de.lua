17cf11015e8a66d680ab490b9f03c3d7efc04568
Wilder MO <66009198+WilderMO@users.noreply.github.com>
2022-01-25 10:53:35 +0100

    Created de.lua
    Added German Translation .lua

---
+++ resources/gcphone/locales/de.lua
@@ -0,0 +1,15 @@
+  Locales['de'] = {
+    ["mission_row"] = "Mission Row Polizeirevier",
+    ["phone_booth"] = "Telefon Zelle",
+    ["key_answer"] = "Drpcke ~INPUT_PICKUP~ um das Telefon Anzunehmen",
+    ["new_message"] = "~o~Neue Nachrichten",
+    ["new_message_from"] = "~o~Neue Nachrichten von ~y~%s",
+    ["new_message_transmitter"] = "~o~Neue Nachrichten von ~g~%s",
+    ["use_fixed"] = "~g~%s's Telefon ~o~(%s) ~n~~INPUT_PICKUP~~w~ Benutze Telefon",
+    
+    -- Social
+    ["new_tweet"] = "Neuer Tweet!",
+
+    -- Warning
+    ["no_phone"] = "YDu hast kein ~r~Telefon~s~."
+}
